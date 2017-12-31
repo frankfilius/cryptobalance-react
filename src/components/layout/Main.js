65f4a766bcb648a8c4c107de52385df59b39e85a
ff05 <frank.filius@gmail.com>
2017-12-31 09:04:12 +0100

    Creat Coins container file

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from 'react'
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, XAxis, YAxis, Tooltip } from 'recharts';
 import styles from './Main.css'
 
 export default class Sidebar extends PureComponent {
   render() {
     return (
       <div className="Main">
+        <ResponsiveContainer width={700} height="80%" >
         <h1>Graph</h1>
+        </ResponsiveContainer>
       </div>
     )
   }
