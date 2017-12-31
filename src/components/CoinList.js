045406b8d20ecd483ab35402b2dc8ecc0572c652
ff05 <frank.filius@gmail.com>
2017-12-31 15:06:36 +0100

    apply styling

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -2,6 +2,7 @@ import React, {PureComponent} from 'react'
 import { connect } from 'react-redux'
 import { action } from '../store'
 import Form from './Form'
+import styles from './CoinList.css'
 
 export class CoinList extends PureComponent {
   constructor(props) {
@@ -43,10 +44,16 @@ export class CoinList extends PureComponent {
     const {coins, myCoins} = this.props
       return (
         <div className='CoinList'>
-          {coins &&
+          <h1>CoinList</h1>
+          <div className="headers">
+            <p>Coin:</p>
+            <p>Amount:</p>
+            <p></p>
+          </div>
+          {coins.length > 0 &&
             <Form content={coins} contentValue="symbol" onChange={this.handleChange} onSubmit={this.handleSubmit} valueSelect={this.state.symbol} valueNumber={this.state.amount} />
           }
-          {myCoins &&
+          {myCoins.length > 0 &&
             myCoins.map((coin, index) => <Form key={index} content={myCoins} contentValue="symbol" onChange={this.handleChange} onClick={this.handleClick(coin)} onSubmit={this.handleSubmit} valueSelect={coin.symbol} valueNumber={coin.amount} />)
           }
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,19 @@ import SelectField from 'material-ui/SelectField'
 import TextField from 'material-ui/TextField'
 import MenuItem from 'material-ui/MenuItem'
 import FlatButton from 'material-ui/FlatButton'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/fontawesome-free-solid'
 import styles from './Form.css'
 
 export default class Form extends PureComponent {
   render() {
    const { content, contentValue, onChange, onClick, onSubmit, valueOption, valueSelect, valueNumber } = this.props
+   let button = null
+   if (!content[0].amount) {
+     button =  <FlatButton className='button' style={{fontSize: 20, width: 30}} hoverColor='transparent' icon={<FontAwesomeIcon id="icon-plus" style={{color: '#C8E6C9'}} icon={faPlus}/>} type='submit' />
+   } else {
+     button = <FlatButton className='button' style={{width: 30}} hoverColor='transparent' icon={<FontAwesomeIcon id="icon-minus" style={{color: '#FFCDD2'}} icon={faMinus}/>} onClick={onClick}/>
+   }
     return (
       <form className="Form" onSubmit={onSubmit}>
         <div>
@@ -16,11 +24,10 @@ export default class Form extends PureComponent {
           </SelectField >
         </div>
         <div>
-          <TextField name="amount" type="number" step="0.00000001" style={{width: 100}} value={valueNumber} onChange={onChange} />
+          <TextField name="amount" type="number" step="0.00000001" style={{minWidth: 80}} value={valueNumber} onChange={onChange} />
         </div>
         <div>
-        <FlatButton secondary={true} style={{ width: 10 }} type="submit" label="✓" />
-        <FlatButton primary={true} style={{ width: 20 }} label="✗" onClick={onClick}/>
+          {button}
         </div>
       </form>
     )
